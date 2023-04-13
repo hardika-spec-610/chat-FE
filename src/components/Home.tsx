a49179241fccdc34617e18e333ac037daf2df835
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-04-13 15:07:09 +0200

    set socket connection

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import {
   Container,
   Row,
@@ -7,12 +7,49 @@ import {
   FormControl,
   ListGroup,
   Button,
-} from "react-bootstrap"
+} from "react-bootstrap";
+import { io } from "socket.io-client";
+import { Message, User } from "../types";
+
+// 1. Every time we refresh the page, we reconnect to the socket.io server
+// 2. If the connection is correctly established, the server will emit to us an event called 'welcome' containing a message with the id of the connection
+// 3. If we want to do something when the event happens we shall LISTEN to that event by using socket.on("welcome", () => {})
+// 4. Once we are connected we would like to submit the username to the server --> we are going to EMIT an event called "setUsername", containing the username itself as payload
+// 5. Server is listening for "setUsername" event, WHEN it receives that event it will send a "loggedIn" event to this client, and broadcast to all the other users another event with the updated list of users
+// 6. The list of online users is updated only during "login", but what happens if a new user joins after the "login"? In this case we are not updating the list
+// 7. When a new user joins server emits an event called "updateOnlineUsersList", this is supposed to update that list when a user joins or leaves
+// 8. When the client wants to send a message, it needs to EMIT an event called "sendMessage", as payload it should contain: sender, text, date
+// 9. Server is listening for "sendMessage" event, when it receives that it will broadcast that message to everybody but the sender by firing an event called "newMessage"
+// 10. Anybody who is listening for a "newMessage" event will receive that message and can then display the content on the page
+
+const socket = io("http://localhost:3001", { transports: ["websocket"] }); // if we don't pass this option, socket.io will try to use polling
 
 const Home = () => {
-  const [username, setUsername] = useState("")
-  const [message, setMessage] = useState("")
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const [onlineUsers, setOnlineUsers] = useState<User[]>([]);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [chatHistory, setChatHistory] = useState<Message[]>([]);
 
+  useEffect(() => {
+    // this code will be executed only once
+    // we want to set our event listeners only once
+    // therefore this is the good place for them
+    socket.on("welcome", (welcomeMessage) => {
+      console.log(welcomeMessage);
+
+      socket.on("loggedIn", (onlineUsersList) => {
+        console.log(onlineUsersList);
+        setOnlineUsers(onlineUsersList);
+        setLoggedIn(true);
+      });
+    });
+  }, []);
+
+  const submitUsername = () => {
+    // here we will be emitting the "setUsername" event (server is already listening for that)
+    socket.emit("setUsername", { username });
+  };
   return (
     <Container fluid>
       <Row style={{ height: "95vh" }} className="my-3">
@@ -21,14 +58,16 @@ const Home = () => {
           {/* TOP AREA: USERNAME INPUT FIELD */}
           {/* {!loggedIn && ( */}
           <Form
-            onSubmit={e => {
-              e.preventDefault()
+            onSubmit={(e) => {
+              e.preventDefault();
+              submitUsername();
             }}
           >
             <FormControl
               placeholder="Set your username here"
               value={username}
-              onChange={e => setUsername(e.target.value)}
+              onChange={(e) => setUsername(e.target.value)}
+              disabled={loggedIn}
             />
           </Form>
           {/* )} */}
@@ -36,14 +75,14 @@ const Home = () => {
           <ListGroup></ListGroup>
           {/* BOTTOM AREA: NEW MESSAGE */}
           <Form
-            onSubmit={e => {
-              e.preventDefault()
+            onSubmit={(e) => {
+              e.preventDefault();
             }}
           >
             <FormControl
               placeholder="Write your message here"
               value={message}
-              onChange={e => setMessage(e.target.value)}
+              onChange={(e) => setMessage(e.target.value)}
             />
           </Form>
         </Col>
@@ -53,7 +92,7 @@ const Home = () => {
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
